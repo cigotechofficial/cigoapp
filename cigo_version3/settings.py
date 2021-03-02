2a78bfea60d516c9fbd518262845f2a41735445a
Ryan <ryan24work@gmail.com>
2021-03-02 11:01:42 +0530

    allowed user to prod

--- cigo_version3/settings.py
+++ cigo_version3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '97quixm#*05p9d5f(w%11f96nutg82dw*+lgdn)bfp$3xi@@oh'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['cigo-staging.eba-sdykin7a.ap-south-1.elasticbeanstalk.com','cigoenv.eba-pfpimnu9.us-west-2.elasticbeanstalk.com', 'www.cigo.co.in', 'cigo.co.in']
+ALLOWED_HOSTS = ['cigoproduction.ap-south-1.elasticbeanstalk.com', 'www.cigo.co.in', 'cigo.co.in']
 # ALLOWED_HOSTS = ['*']
 
 
