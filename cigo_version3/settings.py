0e74aba2beea863ca4545810ed7a2fec64921238
Ryan <ryan24work@gmail.com>
2021-03-10 22:20:41 +0530

    s3 installed

--- cigo_version3/settings.py
+++ cigo_version3/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
 	'allauth.account', 
 	'allauth.socialaccount', 
 	'allauth.socialaccount.providers.google',
+
+	'storages',
 ]
 
 SITE_ID = 2
@@ -175,3 +177,18 @@ AUTHENTICATION_BACKENDS = (
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 DEBUG = False
+
+
+AWS_ACCESS_KEY_ID = 'AKIA4KY24AYIV3KBBJCE'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cigo-bucket'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
