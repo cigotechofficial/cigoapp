baf22c9f133236f19ce2860e435c69c45901174f
Ryan <ryan24work@gmail.com>
2021-04-21 21:38:59 +0530

    a

--- onlymenuapp2/urls.py
+++ onlymenuapp2/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 
 urlpatterns = [
-	path('<int:restaurantidslug>/welcome2/', views.welcome, name='welcome'),
-	path('<int:restaurantidslug>/menu2/', views.customerapp, name='customerapp'),
+	path('<int:restaurantidslug>/welcome/', views.welcome, name='welcome'),
+	path('<int:restaurantidslug>/menu/', views.customerapp, name='customerapp'),
 	
 ]
