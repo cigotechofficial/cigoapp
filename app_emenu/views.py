6aee5b0e2d4ad67f74c7e16823147c1e216cd9a2
Ryan Chowdhury <2ryan.141298@gmail.com>
2022-02-08 20:51:13 +0530

    editted sno. in menu browser and item listing

--- app_emenu/views.py
+++ app_emenu/views.py
@@ -27,8 +27,11 @@ def emenu(request,restaurantidslug):
 	catprods = Menu.objects.filter(venue=restaurantidslug).filter(showtype=1).filter(availability="ava").values('category')
 	cats = {item['category'] for item in catprods}
 
-	for cat in cats:
-		prod = Menu.objects.filter(venue=restaurantidslug).filter(showtype=1).filter(availability="ava").filter(category=cat )
+	catorder_str = Venue.objects.filter(venueid=restaurantidslug)[0].category_list_sorted
+	catorder_list = [c.strip() for c in catorder_str.split(',')]
+
+	for cat in catorder_list:
+		prod = Menu.objects.filter(venue=restaurantidslug).filter(showtype=1).filter(availability="ava").filter(category=cat ).order_by('serialno')
 		allProds.append(prod)
 
 	allcustomisablefields = {}
