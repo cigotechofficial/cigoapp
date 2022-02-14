3a829cacfaeaca6a7364d2f5bc8bb9dde8e0f7a1
Ryan Chowdhury <2ryan.141298@gmail.com>
2022-02-14 23:41:09 +0530

    Trying to upload to eb

--- cigo_version3/settings/prod.py
+++ cigo_version3/settings/prod.py
@@ -2,7 +2,7 @@ from cigo_version3.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['cigoproduction.ap-south-1.elasticbeanstalk.com', 'www.cigo.co.in', 'cigo.co.in']
+ALLOWED_HOSTS = ['cigoprod.ap-south-1.elasticbeanstalk.com', 'www.cigo.co.in', 'cigo.co.in']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
@@ -15,15 +15,21 @@ SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME':'CigoProdDatabase',
-        'NAME':'postgres',
-        'USER':'postgres',
-        'PASSWORD':'rroonnyy',
-        'HOST':'aa687e5wvo7evp.cjiavj7zji11.ap-south-1.rds.amazonaws.com',
-        'PORT':'5432'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         # 'NAME':'CigoProdDatabase',
+#         'NAME':'postgres',
+#         'USER':'postgres',
+#         'PASSWORD':'rroonnyy',
+#         'HOST':'aa687e5wvo7evp.cjiavj7zji11.ap-south-1.rds.amazonaws.com',
+#         'PORT':'5432'
 
-    }
+#     }
+# }
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
